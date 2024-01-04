668a8cd0f79116fde973616e959b8d8114d195ff
EkaterinaRina <catherinemarsiano@gmail.com>
2024-01-04 23:38:49 +0300

    ad src/even.js

---
+++ src/even.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync';
+
+//export const getName = () => {
+	
+//};
+
+export const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const questionAnswer = () => {
+    const nameUser = readlineSync.question('May I have your name? ');
+	console.log('Hello, ' + nameUser + '!');
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const randomNumber = getRandomNumber(1, 101);
+    console.log('Question: ' + randomNumber);
+    const answer = readlineSync.question('Your answer: ');
+    let result;
+    if (randomNumber % 2 === 0) {
+        if (answer === 'yes') {
+            result = 'Correct!';
+        } else {
+            result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + nameUser;
+        }
+        
+    } else if (randomNumber % 2 !== 0) {
+        if (answer === 'no') {
+            result = 'Correct!';
+        } else {
+            result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + nameUser;
+        }
+    }
+   
+};
+
+
+
+
+
+    
