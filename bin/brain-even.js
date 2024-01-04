0e0c664047acbdef812073b274b8a294d67e14f3
EkaterinaRina <catherinemarsiano@gmail.com>
2024-01-05 00:42:24 +0300

    update src/even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { questionAnswer } from "../src/even.js";
 
 console.log('Welcome to the Brain Games!');
--- src/even.js
+++ src/even.js
@@ -14,25 +14,34 @@ export const questionAnswer = () => {
     const nameUser = readlineSync.question('May I have your name? ');
 	console.log('Hello, ' + nameUser + '!');
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    const randomNumber = getRandomNumber(1, 101);
-    console.log('Question: ' + randomNumber);
-    const answer = readlineSync.question('Your answer: ');
-    let result;
-    if (randomNumber % 2 === 0) {
-        if (answer === 'yes') {
-            result = 'Correct!';
-        } else {
-            result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + nameUser;
-        }
+    let i = 0;
+    while (i < 3) {
+        const randomNumber = getRandomNumber(1, 101);
+        console.log('Question: ' + randomNumber);
+        const answer = readlineSync.question('Your answer: ');
+        let result;
+        if (randomNumber % 2 === 0) {
+            if (answer === 'yes') {
+                result = 'Correct!';
+            } else {
+                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + nameUser;
+                console.log(result);
+                return;
+            }
         
-    } else if (randomNumber % 2 !== 0) {
-        if (answer === 'no') {
+        } else if (randomNumber % 2 !== 0) {
+            if (answer === 'no') {
             result = 'Correct!';
-        } else {
-            result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + nameUser;
+            } else {
+                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + nameUser;
+                console.log(result);
+                return;
+            }
         }
+        console.log(result);
+        i += 1;
     }
-   
+    console.log('Congratulations,' + nameUser + '!');
 };
 
 
