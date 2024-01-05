9098540813865c16450f54356e32566e9ab05f89
EkaterinaRina <catherinemarsiano@gmail.com>
2024-01-05 12:50:04 +0300

    add src/random.js and update src/even.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
-	const name = readlineSync.question('May I have your name? ');
-	console.log('Hello, ' + name + '!');
+let nameUser;
+const getName = () => {
+	nameUser = readlineSync.question('May I have your name? ');
+	console.log('Hello, ' + nameUser + '!');
 };
+
+export { getName, nameUser };
--- src/even.js
+++ src/even.js
@@ -1,18 +1,9 @@
 import readlineSync from 'readline-sync';
-
-//export const getName = () => {
-	
-//};
-
-export const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { getRandomNumber } from './random.js';
+import { getName, nameUser } from './cli.js';
 
 export const questionAnswer = () => {
-    const nameUser = readlineSync.question('May I have your name? ');
-	console.log('Hello, ' + nameUser + '!');
+    getName();
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
     let i = 0;
     while (i < 3) {
@@ -24,7 +15,7 @@ export const questionAnswer = () => {
             if (answer === 'yes') {
                 result = 'Correct!';
             } else {
-                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + nameUser;
+                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, " + nameUser + '!';
                 console.log(result);
                 return;
             }
@@ -33,7 +24,7 @@ export const questionAnswer = () => {
             if (answer === 'no') {
             result = 'Correct!';
             } else {
-                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + nameUser;
+                result = "'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.\nLet's try again, " + nameUser + '!';
                 console.log(result);
                 return;
             }
@@ -41,7 +32,7 @@ export const questionAnswer = () => {
         console.log(result);
         i += 1;
     }
-    console.log('Congratulations,' + nameUser + '!');
+    console.log('Congratulations, ' + nameUser + '!');
 };
 
 
