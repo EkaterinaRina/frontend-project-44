2f93b2d3a48e269595d36c7c3a8a8ff2fab2b065
EkaterinaRina <catherinemarsiano@gmail.com>
2024-01-06 15:46:34 +0300

    eslint bugs fix index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let nameUser;
 export const getName = () => {
   console.log('Welcome to the Brain Games!');
   nameUser = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + nameUser + '!');
+  console.log(`Hello, ${nameUser}!`);
 };
 
 export const getGame = (taskGame, startGame) => {
@@ -12,26 +12,18 @@ export const getGame = (taskGame, startGame) => {
   let i = 0;
   while (i < 3) {
     const [answer, question] = startGame();
-    console.log('Question: ' + question);
+    console.log(`Question: ${question}`);
     const answerUser = readlineSync.question('Your answer: ');
     let result;
     if (answerUser === answer) {
       result = 'Correct!';
     } else {
-      result =
-        "'" +
-        answerUser +
-        "'" +
-        " is wrong answer ;(. Correct answer was '" +
-        answer +
-        "'.\nLet's try again, " +
-        nameUser +
-        '!';
+      result = `'${answerUser}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${nameUser}!`;
       console.log(result);
       return;
     }
     console.log(result);
     i += 1;
   }
-  console.log('Congratulations, ' + nameUser + '!');
+  console.log(`Congratulations, ${nameUser}!`);
 };
