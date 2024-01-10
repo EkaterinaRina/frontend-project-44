40678eae86134e219346e2a5f167b2ceb051c752
EkaterinaRina <catherinemarsiano@gmail.com>
2024-01-10 20:06:49 +0300

    change calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,26 +4,23 @@ import getGame from '../index.js';
 const taskGame = 'What is the result of the expression?';
 
 const calculate = (numberOne, numberTwo, symbol) => {
-  let result;
   switch (symbol) {
     case '+':
-      result = numberOne + numberTwo;
-      break;
+      return numberOne + numberTwo;
     case '-':
-      result = numberOne - numberTwo;
-      break;
+      return numberOne - numberTwo;
+    case '*':
+      return numberOne * numberTwo;
     default:
-      result = numberOne * numberTwo;
-      break;
+      console.log(`Unknown symbol ${symbol}`);
   }
-  return result;
 };
 
 const startGame = () => {
   const firstNumber = getRandomNumber(1, 100);
   const secondNumber = getRandomNumber(1, 100);
   const symbols = ['+', '-', '*'];
-  const randomIndex = getRandomNumber(0, 2);
+  const randomIndex = getRandomNumber(0, symbols.length - 1);
   const randomSymbol = symbols[randomIndex];
   const question = `${firstNumber} ${randomSymbol} ${secondNumber}`;
   const result = calculate(firstNumber, secondNumber, randomSymbol);
@@ -31,6 +28,4 @@ const startGame = () => {
   return [answer, question];
 };
 
-export default () => {
-  getGame(taskGame, startGame);
-};
+export default () => { getGame(taskGame, startGame); };
